45838f08b0ea3064e27d3c67c2c43b2c3ac4b87a
Xfoundation <335320963@qq.com>
2018-11-26 10:19:01 +0800

    test eclipse commit and push !

--- src/main/java/com/xurongbiao/foundation/day01/Welcome.java
+++ src/main/java/com/xurongbiao/foundation/day01/Welcome.java
@@ -4,5 +4,7 @@ public class Welcome {
 	public static void main(String [] args) {
 		System.out.println("Welcome to Java !");
 		System.out.println("Hello World !");
+		System.out.println("test eclipse commit and push !");
+		
 	}
 }
